ed23db99832d17f63a831a4730e817b5a4e6e77b
Jaydeepsolanki7 <jay.solanki7694@gmail.com>
2024-03-05 19:03:03 +0530

    added bootstrap in the slices

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
-import { Counter } from '../features/counter/counter'
-import User from '../features/users/user'
+import React from 'react';
+import { Counter } from '../features/counter/Counter'; // Ensure the correct path to Counter component
+import User from '../features/users/User'; // Ensure the correct path to User component
 
 function Home() {
   return (
-    <div>
+    <div className="home-container">
       <h1>This is the home page which contains 2 slices</h1>
-      <Counter/>
-      <User/>
+      <div className="row mt-4">
+        <div className="col-6">
+          <User />
+        </div>
+        <div className="col-6">
+          <Counter />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
